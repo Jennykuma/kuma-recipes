e49952d75d977ae48b1816c40e22b22f6d882e57
Jennykuma <jennyle.dev@gmail.com>
2026-01-14 13:52:16 -0700

    Only change rating if it's different than it's current value

--- apps/web/src/components/RecipeDetails.tsx
+++ apps/web/src/components/RecipeDetails.tsx
@@ -6,15 +6,25 @@ import Rating from './Rating';
 const RecipeDetails = () => {
     const { id } = useParams();
     const { recipe } = useRecipeDetails(id || '');
-    const { mutate: updateRating } = useRecipeRating(id || '');
+    const { mutate: updateRating, isPending } = useRecipeRating(id || '');
+
+    const handleChangeRating = (rating: number) => {
+        if (!recipe) {
+            return;
+        }
+
+        if (recipe?.rating !== rating) {
+            updateRating(rating);
+        }
+    };
 
     return (
         <div>
             <span>{recipe?.title}</span>
             <Rating
                 rating={recipe?.rating}
-                onChange={(rating: number) => updateRating(rating)}
-                interactive
+                onChange={handleChangeRating}
+                interactive={!isPending}
             />
         </div>
     );
--- apps/web/src/main.tsx
+++ apps/web/src/main.tsx
@@ -3,6 +3,7 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { RouterProvider } from 'react-router-dom';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import router from './routes.tsx';
 import './index.css';
 
@@ -12,6 +13,7 @@ createRoot(document.getElementById('root')!).render(
     <StrictMode>
         <QueryClientProvider client={queryClient}>
             <RouterProvider router={router} />
+            <ReactQueryDevtools />
         </QueryClientProvider>
     </StrictMode>
 );
