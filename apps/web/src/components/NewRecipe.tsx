71f8abadb65990bfe57b82f1f13e9f9fbbd3b77a
Jennykuma <jennyle.dev@gmail.com>
2026-01-20 13:30:54 -0700

    Add remake and notes section

--- apps/web/src/components/NewRecipe.tsx
+++ apps/web/src/components/NewRecipe.tsx
@@ -30,18 +30,36 @@ const NewRecipe = () => {
                             <input
                                 id="title"
                                 type="text"
-                                className="border border-gray-200 w-full mb-8"
+                                className="border border-gray-200 w-full mb-4"
                                 {...register('title')}
                             ></input>
 
                             <label htmlFor="rating">Rating</label>
                             <Rating interactive />
+
+                            <label htmlFor="remake" className="block mt-4">
+                                Remake?
+                                <input
+                                    type="checkbox"
+                                    className="ml-2"
+                                    {...register('remake')}
+                                />
+                            </label>
+
+                            <label htmlFor="notes" className="block mt-4">
+                                Notes
+                            </label>
+                            <textarea
+                                className="w-full resize-none border border-gray-200 rounded-sm p-2"
+                                rows={4}
+                                placeholder="Notes (e.g. too sweet, bake 2 min longer next time)"
+                            ></textarea>
                         </div>
 
                         <div className="w-5/8 p-8 border border-gray-200 rounded-sm shadow-sm">
                             <label htmlFor="ingredients">Ingredients</label>
                             <IngredientTable />
-
+                            <hr className="mt-8 border-gray-200 border-t-1" />
                             <label htmlFor="steps" className="block mt-8">
                                 Steps
                             </label>
