463fd83e0fe8776b4e1ec113cc1ef1320759cc3e
Jennykuma <jennyle.dev@gmail.com>
2026-06-10 14:42:18 -0600

    Fix ingredients bullet point spacing

--- apps/web/src/pages/NewRecipe/components/IngredientsTable.tsx
+++ apps/web/src/pages/NewRecipe/components/IngredientsTable.tsx
@@ -119,7 +119,7 @@ const IngredientsTable = ({
                         <div key={field.id} className="flex items-center">
                             <Circle
                                 className={classNames(
-                                    'w-8 h-1.5',
+                                    'w-3 h-1.5',
                                     activeIndex === index
                                         ? 'text-gray-600'
                                         : 'text-gray-400'
@@ -127,7 +127,7 @@ const IngredientsTable = ({
                             />
                             <input
                                 className={classNames(
-                                    'w-full text-sm rounded-md border w-100 transition-colors focus:outline-none dark:bg-[#252525] dark:text-gray-100',
+                                    'w-full ml-1 text-sm rounded-md border w-100 transition-colors focus:outline-none dark:bg-[#252525] dark:text-gray-100',
                                     activeIndex === index
                                         ? 'border-sage-300 dark:border-sage-300'
                                         : 'border-gray-200 dark:border-gray-600'
